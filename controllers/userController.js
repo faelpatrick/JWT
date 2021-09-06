b3b3d82bc526281ff1ae9ffa7458a7a8ad578de3
faelpatrick <faelpatrick@gmail.com>
2021-09-06 17:38:56 -0300

    First commit Learning Mongo DB

---
+++ controllers/userController.js
@@ -0,0 +1,13 @@
+const userController = {
+
+    register: function(req, res) {
+        console.log('Register')
+        res.send('Register')
+    },
+
+    login: function(req, res) {
+        console.log('Login')
+        res.send('Login')
+    }
+}
+module.exports = userController;
